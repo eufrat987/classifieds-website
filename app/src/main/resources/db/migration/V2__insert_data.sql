315e9452a3543d6e81d2bf64378fc0d8772e4d56
eufrat <lukzyw@gmail.com>
2025-08-23 19:18:19 +0200

    Fix publication table

--- app/src/main/resources/db/migration/V2__insert_data.sql
+++ app/src/main/resources/db/migration/V2__insert_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO publication (description, creationDate, views) 
+INSERT INTO publication (description, creation_date, views) 
 VALUES ('text1', '2025-08-23 12:35:12', 0), ('text2','2025-08-23 12:35:12', 0);
