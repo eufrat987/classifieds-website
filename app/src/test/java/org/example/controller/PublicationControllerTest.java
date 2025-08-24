6931b58b05678ff5e039af404c73221328393a1b
eufrat <lukzyw@gmail.com>
2025-08-24 18:48:51 +0200

    Fix controller tests

--- app/src/test/java/org/example/controller/PublicationControllerTest.java
+++ app/src/test/java/org/example/controller/PublicationControllerTest.java
@@ -8,21 +8,21 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 
-import org.example.repository.PublicationRepository;
+import org.example.service.PublicationService;
 import org.example.model.Publication;
-import java.util.Optional;
 
 @WebMvcTest(PublicationController.class)
 public class PublicationControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	@MockBean
-	private PublicationRepository publicationRepository; 
+	private PublicationService publicationService; 
 	
 	@Test
 	public void testGetPublicationById() throws Exception {
-		Mockito.when(publicationRepository.findById(Mockito.any())).thenReturn(Optional.of(new Publication()));
+		Mockito.when(publicationService.getPublicationAndIncrementViews(1L)).thenReturn(new Publication());
 
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/publications/1"))
 			.andExpect(MockMvcResultMatchers.status().isOk());
@@ -30,25 +30,27 @@ public class PublicationControllerTest {
 
 	@Test
 	public void testDeletePublicationById() throws Exception {
-		Mockito.when(publicationRepository.findById(Mockito.any())).thenReturn(Optional.of(new Publication()));
-
 		mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/publications/1"))
-			.andExpect(MockMvcResultMatchers.status().isOk());
+			.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 
 	@Test
 	public void testCreatePublication() throws Exception {
-		Mockito.when(publicationRepository.findById(Mockito.any())).thenReturn(Optional.of(new Publication()));
+		Mockito.when(publicationService.createPublication(Mockito.any())).thenReturn(new Publication());
 
-		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/publications/1"))
+		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/publications")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content("{\"description\": \"desc\"}"))
 			.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 	@Test
 	public void testUpdatePublicationById() throws Exception {
-		Mockito.when(publicationRepository.findById(Mockito.any())).thenReturn(Optional.of(new Publication()));
+		Mockito.when(publicationService.updatePublication(Mockito.eq(1L), Mockito.any())).thenReturn(new Publication());
 
-		mockMvc.perform(MockMvcRequestBuilders.patch("/api/v1/publications/1"))
+		mockMvc.perform(MockMvcRequestBuilders.patch("/api/v1/publications/1")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content("{\"description\": \"new one\"}"))
 			.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 }
